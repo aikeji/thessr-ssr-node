4d8ee427975450c92fca626404c100d333b6da7d
Tony Zou <tony200303.zt@gmail.com>
2018-04-29 22:03:47 +0800

    Update update-443.sh

--- update-443.sh
+++ update-443.sh
@@ -2,3 +2,4 @@ sed -i "s#'zhaoj.in'#'wns.windows.com'#" /shadowsocks/userapiconfig.py
 firewall-cmd --add-port=443/tcp --permanent
 firewall-cmd --add-port=443/udp --permanent
 firewall-cmd --reload
+supervisorctl restart mu
