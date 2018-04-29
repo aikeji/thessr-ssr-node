3c7e12ac49496619d536e6d4a05c47de07fd575c
Tony Zou <tony200303.zt@gmail.com>
2018-04-29 22:09:35 +0800

    Delete update-443.sh

--- update-443.sh
+++
@@ -1,5 +0,0 @@
-sed -i "s#'zhaoj.in'#'wns.windows.com'#" /shadowsocks/userapiconfig.py
-firewall-cmd --add-port=443/tcp --permanent
-firewall-cmd --add-port=443/udp --permanent
-firewall-cmd --reload
-supervisorctl restart mu
