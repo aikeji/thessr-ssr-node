9e9c79ffe96d03f02c67a0faff710b5335f4843c
Tony Zou <tony200303.zt@gmail.com>
2018-04-29 22:02:59 +0800

    Add files via upload

---
+++ update-443.sh
@@ -0,0 +1,4 @@
+sed -i "s#'zhaoj.in'#'wns.windows.com'#" /shadowsocks/userapiconfig.py
+firewall-cmd --add-port=443/tcp --permanent
+firewall-cmd --add-port=443/udp --permanent
+firewall-cmd --reload
