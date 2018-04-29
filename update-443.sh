020343af21991d8cf3497b801bbbefb573df6cfc
爱科技博客 <stmzl999@gmail.com>
2018-04-29 23:18:01 +0800

    Update update-443.sh

--- update-443.sh
+++ update-443.sh
@@ -1,4 +1,4 @@
-sed -i "s#'zhaoj.in'#'wns.windows.com'#" /shadowsocks/userapiconfig.py
+sed -i "s#'zhaoj.in'#'wns.windows.com'#" root/shadowsocks/userapiconfig.py
 firewall-cmd --add-port=443/tcp --permanent
 firewall-cmd --add-port=443/udp --permanent
 firewall-cmd --reload
