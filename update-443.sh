d998c2788b4eb36268edfebfaed28efa75da19d9
0x20b <stmzl999@gmail.com>
2018-05-15 11:44:06 +0800

    Update update-443.sh
    Fix v2

--- update-443.sh
+++ update-443.sh
@@ -1,4 +1,5 @@
 sed -i "s#'zhaoj.in'#'wns.windows.com'#" /root/shadowsocks/userapiconfig.py
+sed -i "s#'zhaoj.in'#'wns.windows.com'#" /shadowsocks/userapiconfig.py
 firewall-cmd --add-port=443/tcp --permanent
 firewall-cmd --add-port=443/udp --permanent
 firewall-cmd --reload
