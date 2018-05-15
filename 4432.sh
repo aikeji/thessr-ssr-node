a6d78746a743fbdc6b680de47b38b0e43a9c11ed
0x20b <stmzl999@gmail.com>
2018-05-15 12:18:00 +0800

    Create 4432.sh

---
+++ 4432.sh
@@ -0,0 +1,7 @@
+sed -i "s#'zhaoj.in'#'wns.windows.com'#" /shadowsocks/userapiconfig.py
+firewall-cmd --add-port=443/tcp --permanent
+firewall-cmd --add-port=443/udp --permanent
+firewall-cmd --reload
+supervisorctl restart mu
+service iptables stop
+chkconfig iptables off
